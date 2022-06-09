6576477f1177d6b68d1dc8941dc3aaef796dc7da
xiliangjianke <biankairui@163.com>
2022-06-09 22:53:50 +0800

    Extend the trace start location

--- Source/Blaster/BlasterComponents/CombatComponent.cpp
+++ Source/Blaster/BlasterComponents/CombatComponent.cpp
@@ -223,6 +223,13 @@ void UCombatComponent::TraceUnderCrosshairs(FHitResult& TraceHitResult)
 	{
 		FVector Start = CrosshairWorldPosition;
 
+		if (Character)
+		{
+			float DistanceToCharacter = (Character->GetActorLocation() - Start).Size();
+			Start += CrosshairWorldDirection * (DistanceToCharacter + 100.f);
+			//DrawDebugSphere(GetWorld(), Start, 16.f, 12, FColor::Red, false);
+		}
+
 		FVector End = Start + CrosshairWorldDirection * TRACE_LENGTH;
 
 		GetWorld()->LineTraceSingleByChannel(
